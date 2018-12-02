c3aa6e2240cbe938fe53a165e1d03a87f8195911
icydoge <icydoge@gmail.com>
2018-12-02 10:18:34 +0000

    Add solution for Day 2

---
+++ solutions/day2/day2.go
@@ -0,0 +1,84 @@
+package day2
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+	"strings"
+)
+
+func Run(input string) string {
+	var inputLines = strings.Split(input, "\n")
+	for k, v := range inputLines {
+		inputLines[k] = strings.Trim(v, " \t")
+	}
+	return fmt.Sprintf("Part 1: %v\nPart 2: %v\n", part1(inputLines), part2(inputLines))
+}
+
+func part1(inputLines []string) string {
+	var twoAccumulator int64
+	var threeAccumulator int64
+	for _, line := range inputLines {
+		if len(line) == 0 {
+			continue
+		}
+
+		letterCounter := map[rune]int{}
+		for _, c := range line {
+			val, ok := letterCounter[c]
+			if ok {
+				letterCounter[c] = val + 1
+			} else {
+				letterCounter[c] = 1
+			}
+		}
+
+		hasTwo := false
+		hasThree := false
+		for _, v := range letterCounter {
+			if v == 2 {
+				hasTwo = true
+			} else if v == 3 {
+				hasThree = true
+			}
+		}
+
+		if hasTwo {
+			twoAccumulator++
+		}
+		if hasThree {
+			threeAccumulator++
+		}
+	}
+
+	return strconv.FormatInt(twoAccumulator*threeAccumulator, 10)
+}
+
+func part2(inputLines []string) string {
+	for k, line1 := range inputLines {
+		for _, line2 := range inputLines[k:] {
+			if len(line1) != len(line2) {
+				log.Fatal("Mismatch line length, violating question assumption")
+			}
+			textDistance := 0
+			lastMismatchIndex := 0
+			for k, c := range line1 {
+				if line2[k] != byte(c) {
+					textDistance++
+					lastMismatchIndex = k
+				}
+				if textDistance == 2 {
+					break
+				}
+			}
+
+			if textDistance != 1 {
+				continue
+			} else {
+				return line1[:lastMismatchIndex] + line1[lastMismatchIndex+1:]
+			}
+		}
+	}
+
+	return "(Not Found)"
+}
--- solutions/runner.go
+++ solutions/runner.go
@@ -4,10 +4,12 @@ import (
 	"time"
 
 	"github.com/icydoge/AoC2018/solutions/day1"
+	"github.com/icydoge/AoC2018/solutions/day2"
 )
 
 var runMap = map[string]interface{}{
 	"1": day1.Run,
+	"2": day2.Run,
 }
 
 func RunSolution(solution string, input string) (string, time.Duration) {
@@ -17,7 +19,7 @@ func RunSolution(solution string, input string) (string, time.Duration) {
 	if f, ok := runMap[solution]; ok {
 		results = f.(func(string) string)(input)
 	} else {
-		results = "(Solution not found)"
+		results = "(Solution code not found)"
 	}
 	realExecutionTime := time.Now().Sub(start)
 	return results, realExecutionTime
